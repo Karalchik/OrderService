82a3f7397e703b8a3f5a92a8f91f08fc10e1d298
Mykyta <ddtera636@gmail.com>
2026-04-24 16:29:52 +0200

    fix: docker + frontend

--- OrderUI/src/app/app.routes.ts
+++ OrderUI/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { OrderList } from './components/order-list/order-list';
 import { OrderCreate } from './components/order-create/order-create';
+import { OrderDetails } from './components/order-details/order-details';
 
 export const routes: Routes = [
   {
@@ -12,7 +13,11 @@ export const routes: Routes = [
     path: 'orders/create',
     component: OrderCreate ,
     title: 'Order Creation'
-    },
+  },
+  { 
+    path: 'orders/:id', 
+    component: OrderDetails 
+  },
   {
     path: '',
     redirectTo: 'orders',
